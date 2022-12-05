21c7f6260ce5b0ace0627db8271ec36eab34d18b
mianjoto <mianjoto@gmail.com>
2022-12-04 22:41:00 -0500

    Removing [SerializeField] attrs from public fields

--- Assets/Scripts/Systems/InputListener.cs
+++ Assets/Scripts/Systems/InputListener.cs
@@ -5,29 +5,18 @@ public class InputListener : MonoBehaviour
 {
     #region Keybinds
     [Header("Interaction Keybinds")]
-        [SerializeField]
         public KeyCode LeftClick = KeyCode.Mouse0;
-        [SerializeField]
         public KeyCode RightClick = KeyCode.Mouse1;
-        [SerializeField]
         public KeyCode MiddleMouseClick = KeyCode.Mouse2;
-        [SerializeField]
         public KeyCode InteractKey = KeyCode.E;
-        [SerializeField]
         public KeyCode AlternateInteractKey = KeyCode.F;
-        [SerializeField]
         public KeyCode InventoryKey = KeyCode.Tab;
-        [SerializeField]
         public KeyCode EscapeKey = KeyCode.Escape;
 
     [Header("Movement Keybinds")]
-        [SerializeField]
         public KeyCode MoveUpKey = KeyCode.W;
-        [SerializeField]
         public KeyCode MoveRightKey = KeyCode.D;
-        [SerializeField]
         public KeyCode MoveDownKey = KeyCode.S;
-        [SerializeField]
         public KeyCode MoveLeftKey = KeyCode.A;
     #endregion
 
