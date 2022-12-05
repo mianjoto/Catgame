7bb3b6b7b976337e41db0669e272788d7dd3bf5f
mianjoto <mianjoto@gmail.com>
2022-12-04 23:09:46 -0500

    Adding invoking trigger for each keybind up/down event

--- Assets/Scripts/Systems/InputListener.cs
+++ Assets/Scripts/Systems/InputListener.cs
@@ -5,9 +5,6 @@ public class InputListener : MonoBehaviour
 {
     #region Keybinds
     [Header("Interaction Keybinds")]
-        public KeyCode LeftClick = KeyCode.Mouse0;
-        public KeyCode RightClick = KeyCode.Mouse1;
-        public KeyCode MiddleMouseClick = KeyCode.Mouse2;
         public KeyCode InteractKey = KeyCode.E;
         public KeyCode AlternateInteractKey = KeyCode.F;
         public KeyCode InventoryKey = KeyCode.Tab;
@@ -21,8 +18,80 @@ public class InputListener : MonoBehaviour
     #endregion
 
     #region Events
-    [Header("Input Events")]
-
-
+    [Header("Input Interaction Events")]
+        public static Action OnLeftClickDown;
+        public static Action OnLeftClickUp;
+        public static Action OnRightClickDown;
+        public static Action OnRightClickUp;
+        public static Action OnMiddleMouseClickDown;
+        public static Action OnMiddleMouseClickUp;
+        public static Action OnInteractKeyDown;
+        public static Action OnInteractKeyUp;
+        public static Action OnAlternateInteractKeyDown;
+        public static Action OnAlternateInteractKeyUp;
+        public static Action OnInventoryKeyDown;
+        public static Action OnEscapeKeyDown;
+    [Header("Input Movement Events")]
+        public static Action OnMoveUpKeyDown;
+        public static Action OnMoveUpKeyUp;
+        public static Action OnMoveRightKeyDown;
+        public static Action OnMoveRightKeyUp;
+        public static Action OnMoveDownKeyDown;
+        public static Action OnMoveDownKeyUp;
+        public static Action OnMoveLeftKeyDown;
+        public static Action OnMoveLeftKeyUp;
     #endregion
+
+
+    private static void ListenForKeyDown(KeyCode key, Action onKeyDownEvent)
+    {
+        if (Input.GetKeyDown(key))
+            onKeyDownEvent?.Invoke();
+    }
+
+    private static void ListenForKeyUpAndDown(KeyCode key, Action onKeyDownEvent, Action onKeyUpEvent)
+    {
+        if (Input.GetKeyDown(key))
+            onKeyDownEvent?.Invoke();
+        if (Input.GetKeyUp(key))
+            onKeyUpEvent?.Invoke();
+    }
+
+    private static void ListenForMouseUpAndDown(int mouseKey, Action onMouseDownEvent, Action onMouseUpEvent)
+    {
+        if (Input.GetMouseButtonDown(mouseKey))
+            onMouseDownEvent?.Invoke();
+        if (Input.GetMouseButtonUp(mouseKey))
+            onMouseUpEvent?.Invoke();
+    }
+
+    private void Update()
+    {
+        ListenForUserInteractionKeys();
+        ListenForUserMovementKeys();
+    }
+
+    private void ListenForUserInteractionKeys()
+    {
+        ListenForClicks();
+        ListenForKeyUpAndDown(InteractKey, OnInteractKeyDown, OnInteractKeyUp);
+        ListenForKeyUpAndDown(AlternateInteractKey, OnAlternateInteractKeyDown, OnAlternateInteractKeyUp);
+        ListenForKeyDown(InventoryKey, OnInventoryKeyDown);
+        ListenForKeyDown(EscapeKey, OnEscapeKeyDown);
+    }
+
+    private void ListenForClicks()
+    {
+        ListenForMouseUpAndDown(mouseKey: 0, OnLeftClickDown, OnLeftClickUp);
+        ListenForMouseUpAndDown(mouseKey: 1, OnRightClickDown, OnRightClickUp);
+        ListenForMouseUpAndDown(mouseKey: 2, OnMiddleMouseClickDown, OnMiddleMouseClickUp);
+    }
+
+    private void ListenForUserMovementKeys()
+    {
+        ListenForKeyUpAndDown(MoveUpKey, OnMoveUpKeyDown, OnMoveUpKeyUp);
+        ListenForKeyUpAndDown(MoveRightKey, OnMoveRightKeyDown, OnMoveRightKeyUp);
+        ListenForKeyUpAndDown(MoveDownKey, OnMoveDownKeyDown, OnMoveDownKeyUp);
+        ListenForKeyUpAndDown(MoveLeftKey, OnMoveLeftKeyDown, OnMoveLeftKeyUp);
+    }
 }
