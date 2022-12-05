70f31039fddb1fc814608050c1764171ccfc82d9
mianjoto <mianjoto@gmail.com>
2022-12-04 22:37:45 -0500

    Adding keybinds to InputListener

--- Assets/Scripts/Systems/InputListener.cs
+++ Assets/Scripts/Systems/InputListener.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputListener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Keybinds
+    [Header("Interaction keybinds")]
+        [SerializeField]
+        public KeyCode LeftClick = KeyCode.Mouse0;
+        [SerializeField]
+        public KeyCode RightClick = KeyCode.Mouse1;
+        [SerializeField]
+        public KeyCode MiddleMouseClick = KeyCode.Mouse2;
+        [SerializeField]
+        public KeyCode InteractKey = KeyCode.E;
+        [SerializeField]
+        public KeyCode AlternateInteractKey = KeyCode.F;
+        [SerializeField]
+        public KeyCode InventoryKey = KeyCode.Tab;
+        [SerializeField]
+        public KeyCode EscapeKey = KeyCode.Escape;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Movement keybinds")]
+        [SerializeField]
+        public KeyCode MoveUpKey = KeyCode.W;
+        [SerializeField]
+        public KeyCode MoveRightKey = KeyCode.D;
+        [SerializeField]
+        public KeyCode MoveDownKey = KeyCode.S;
+        [SerializeField]
+        public KeyCode MoveLeftKey = KeyCode.A;
+    #endregion
 }
