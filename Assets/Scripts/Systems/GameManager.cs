2922a69614e069d6dcdcac0e8392d9d0a7d54ce3
mianjoto <mianjoto@gmail.com>
2022-12-10 14:32:57 -0500

    Adding singleton enforcement to GameManager

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -1,18 +1,35 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] public static GameObject Player;
+    #region SINGLETONS
+        private static GameManager _instance;
+        public static GameManager Instance { get { return _instance; } }
+        [SerializeField] public static GameObject Player;
+    #endregion
 
-    void Awake()
+    private void Awake()
+    {
+        HandleSingleton();
+        HandleDontDestorys();
+    }
+
+    void HandleSingleton()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    private void HandleDontDestorys()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
 
         DontDestroyOnLoad(this);
         DontDestroyOnLoad(Player);
-    }
-
-    void Start()
-    {
     }
 }
